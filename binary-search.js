ccd142cb6228e773bd52136d1de40f95471e4b6e
I_am_rohan <88994067+Rohanverma4@users.noreply.github.com>
2022-04-07 20:20:18 +0530

    lower-upper bounds

--- binary-search.js
+++ binary-search.js
@@ -2,15 +2,18 @@
 const binarySearch = (k,array) => {
     let left_index = 0;
     let right_index = array.length - 1;
-    while (left_index < right_index){
-        const mid = left + Math.floor((right_index - left_index) / 2);
-        if (array[mid] === k){
+    while (left_index <= right_index){
+        const mid = left_index + Math.floor((right_index - left_index) / 2);
+        if (array[mid] == k){
             return mid;
-        } else if (array[mid] < k){
-            left_index = mid + 1;
+        } else if (array[mid] > k){
+            right_index = mid - 1;
         } else {
-            right_index = mid - 1
+            left_index = mid + 1;
         }
     }
     return -1;
 }
+var k = 5;
+var arr = [1,3,4,5,8,9,15,30,67]
+console.log(binarySearch(k,arr))
---
+++ upper-bound.js
@@ -0,0 +1,16 @@
+
+const upperBound = (k,array) => {
+    let left_index = 0;
+    let right_index = array.length - 1;
+    while(left_index < right_index){
+        const mid = left_index + Math.floor((right_index - left_index)/2);
+        if (array[mid] > k){
+            right_index = mid;
+        } else {
+            left_index = mid+1;
+        }
+    }
+    return left_index;
+}
+
+console.log(upperBound(1,[0, 1, 1, 1, 2]))

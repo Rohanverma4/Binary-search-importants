103798fbd8cc1a0176ea7ffffe6b2e4e82d4c6d7
I_am_rohan <88994067+Rohanverma4@users.noreply.github.com>
2022-04-08 16:54:46 +0530

    Last-occurance

--- last-occurance.js
+++ last-occurance.js
@@ -2,13 +2,13 @@ const lastOccurance = (k,array) => {
     let left_index = 0;
     let right_index = array.length - 1;
     let res = -1;
-    while (left_index < right_index){
+    while (left_index <= right_index){
         const mid = left_index + Math.floor((right_index-left_index)/2)
         if (array[mid] === k){
             res = mid;
             left_index = mid+1;
         }
-        if(k > array[mid]){
+        else if(k > array[mid]){
             left_index = mid + 1;
         } else {
             right_index = mid - 1;

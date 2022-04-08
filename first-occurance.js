cd049694cf647006487c17502fad223004578a39
I_am_rohan <88994067+Rohanverma4@users.noreply.github.com>
2022-04-08 16:56:28 +0530

    first-occurance

---
+++ first-occurance.js
@@ -0,0 +1,21 @@
+const firstOccurance = (k,array) => {
+    let left_index = 0;
+    let right_index = array.length - 1;
+    let res = -1;
+    while (left_index <= right_index){
+        const mid = left_index + Math.floor((right_index-left_index)/2)
+        if (array[mid] === k){
+            res = mid;
+            right_index = mid-1;
+        }
+        else if(k > array[mid]){
+            left_index = mid + 1;
+        } else {
+            right_index = mid - 1;
+        }
+    }
+    return res;
+}
+
+const arr = [1,2,5,5,6,9,12,67,124,567]
+console.log(firstOccurance(5,arr))
